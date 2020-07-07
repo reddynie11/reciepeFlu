737142cf3ee3cb5b6b0c1d36f9d1724775e4f94f
reddynie11 <reddy.nie11@gmail.com>
2020-07-07 21:08:12 +0530

    pageNavigation

---
+++ lib/screens/category_meal_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text('Reciepe'),),
+      body: Center(
+       child: Text('THe Category Reciepe page')
+      ) ,
+    );
+  }
+}
--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
 
+import '../screens/category_meal_screen.dart';
+
 class CategoryGridItem extends StatelessWidget {
   final String title;
   final Color color;
 
   CategoryGridItem(this.title, this.color);
+  
+  void selectedCategory(BuildContext context){
+    Navigator.of(context).push(MaterialPageRoute(builder: (_){
+      return CategoryMealScreen();
+    } ));
+  }
 
-  @override 
-  Widget build(BuildContext context){
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Text(title, style: Theme.of(context).textTheme.headline6,),
-      //color: color,
-      decoration: BoxDecoration(color:color,borderRadius: BorderRadius.circular(10)),
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectedCategory(context),
+      child: Container(
+        padding: EdgeInsets.all(25),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        //color: color,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.5), color],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight)),
+      ),
     );
   }
 }
